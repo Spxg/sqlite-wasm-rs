05fff9d92cc4a916d1a27b73fa5d80d40861607a
Spxg <unsafe@outlook.es>
2025-01-12 14:36:41 +0800

    remove unused export

--- sqlite-wasm/index.mjs
+++ sqlite-wasm/index.mjs
@@ -1,7 +1,3 @@
 import { default as sqlite3InitModule } from './sqlite-wasm/jswasm/sqlite3-bundler-friendly.mjs';
-import './sqlite-wasm/jswasm/sqlite3-worker1-promiser.mjs';
-
-const sqlite3Worker1Promiser = self.sqlite3Worker1Promiser;
 
 export default sqlite3InitModule;
-export { sqlite3Worker1Promiser };
