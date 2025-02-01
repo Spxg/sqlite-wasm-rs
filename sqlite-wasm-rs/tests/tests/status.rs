bdadd9a2ef3448c7701ff3d68f21a5ed225b9719
Spxg <unsafe@outlook.es>
2025-02-01 13:45:50 +0800

    Update status tests

--- sqlite-wasm-rs/tests/tests/status.rs
+++ sqlite-wasm-rs/tests/tests/status.rs
@@ -8,7 +8,7 @@ async fn test_memory_used() {
     unsafe {
         let used = sqlite3_memory_used();
         console_log!("memory used: {used}");
-        assert!(used > 0);
+        assert!(used >= 0);
     }
 }
 
@@ -19,7 +19,7 @@ async fn test_highwater() {
     unsafe {
         let highwater = sqlite3_memory_highwater(0);
         console_log!("memory highwater: {highwater}");
-        assert!(highwater > 0);
+        assert!(highwater >= 0);
     }
 }
 
@@ -30,14 +30,15 @@ async fn test_status() {
     unsafe {
         let mut used = 0;
         let mut highwater = 0;
-        sqlite3_status(
+        let ret = sqlite3_status(
             SQLITE_STATUS_MEMORY_USED,
             &mut used as _,
             &mut highwater as _,
             0,
         );
+        assert_eq!(ret, SQLITE_OK);
         console_log!("memory status: used: {used} highwater: {highwater}");
-        assert!(used > 0);
-        assert!(highwater > 0);
+        assert!(used >= 0);
+        assert!(highwater >= 0);
     }
 }
