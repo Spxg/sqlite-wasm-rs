c5a06ecfba59aa763840591cceb643edf558247a
Spxg <unsafe@outlook.es>
2025-06-28 01:23:00 +0800

    Rename tests folder

--- sqlite-wasm-rs/tests/full/vfs/sahpool.rs
+++ sqlite-wasm-rs/tests/full/vfs/sahpool.rs
@@ -2,7 +2,7 @@ use sqlite_wasm_rs::sahpool_vfs::{install as install_opfs_sahpool, *};
 use sqlite_wasm_rs::*;
 use wasm_bindgen_test::wasm_bindgen_test;
 
-use crate::allin::{check_persistent, prepare_simple_db};
+use crate::full::{check_persistent, prepare_simple_db};
 
 #[wasm_bindgen_test]
 async fn test_opfs_sah_vfs_default() {
