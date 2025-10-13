131d0e11961a7cbf4fe20535a472dde172d8d679
Spxg <unsafe@outlook.es>
2025-10-13 23:08:09 +0800

    Add changelog for `#117`

--- src/vfs/mod.rs
+++ src/vfs/mod.rs
@@ -18,3 +18,17 @@ pub unsafe extern "C" fn sqlite3_os_end() -> std::ffi::c_int {
     memory::uninstall();
     SQLITE_OK
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{sqlite3_initialize, sqlite3_shutdown, SQLITE_OK};
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[wasm_bindgen_test]
+    fn test_initialize_shutdown() {
+        unsafe {
+            assert_eq!(sqlite3_initialize(), SQLITE_OK, "failed to initialize");
+            assert_eq!(sqlite3_shutdown(), SQLITE_OK, "failed to shutdown");
+        }
+    }
+}
