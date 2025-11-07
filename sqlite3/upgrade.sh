0135bc82b975965ff386442cb103311dcde62a8a
Spxg <unsafe@outlook.es>
2025-11-07 13:45:17 +0800

    Bump sqlite3 and sqlite3mc versions (#139)
    * Bump sqlite3 verion to 3.51.0
    
    * Bump sqlite3mc verion to 2.2.5
    
    * Update precompiled library and bindings
    https://github.com/Spxg/sqlite-wasm-rs/actions/runs/19159310226
    
    ---------
    
    Co-authored-by: Spxg <17252397+Spxg@users.noreply.github.com>

--- sqlite3/upgrade.sh
+++ sqlite3/upgrade.sh
@@ -1,4 +1,4 @@
-SQLITE=sqlite-amalgamation-3500400
+SQLITE=sqlite-amalgamation-3510000
 curl -O https://sqlite.org/2025/$SQLITE.zip
 unzip -p "$SQLITE.zip" "$SQLITE/sqlite3.c" > "sqlite3.c"
 unzip -p "$SQLITE.zip" "$SQLITE/sqlite3.h" > "sqlite3.h"
