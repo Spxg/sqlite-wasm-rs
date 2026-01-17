0b42ecbb5922797299ad45f7a55455d93de895ac
Spxg <unsafe@outlook.es>
2026-01-17 19:31:52 +0800

    Bump sqlite3 version to 3.51.2 (#168)

--- script/sqlite3_upgrade.sh
+++ script/sqlite3_upgrade.sh
@@ -1,5 +1,5 @@
-SQLITE=sqlite-amalgamation-3510100
-curl -O https://sqlite.org/2025/$SQLITE.zip
+SQLITE=sqlite-amalgamation-3510200
+curl -O https://sqlite.org/2026/$SQLITE.zip
 unzip -p "$SQLITE.zip" "$SQLITE/sqlite3.c" > "sqlite3/sqlite3.c"
 unzip -p "$SQLITE.zip" "$SQLITE/sqlite3.h" > "sqlite3/sqlite3.h"
 unzip -p "$SQLITE.zip" "$SQLITE/sqlite3ext.h" > "sqlite3/sqlite3ext.h"
