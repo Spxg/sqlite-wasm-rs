38a19c90f27ddc7d2f129b10f8117bf82ce0dd70
Luca <cappelletti.luca94@gmail.com>
2026-01-30 07:09:12 +0100

    Further cleaning of PR

--- examples/nodejs/index.js
+++
@@ -1,6 +0,0 @@
-if (!globalThis.crypto) {
-    globalThis.crypto = require("node:crypto").webcrypto;
-}
-
-const wasm = require('./pkg/nodejs.js');
-// wasm.main(); // main is executed automatically due to #[wasm_bindgen(start)]
