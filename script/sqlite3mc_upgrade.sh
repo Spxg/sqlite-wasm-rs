2ababfbea2d39709b283c9268f393a182c14e5a0
Spxg <unsafe@outlook.es>
2026-06-11 21:38:23 +0800

    Bump SQLite version

--- script/sqlite3mc_upgrade.sh
+++ script/sqlite3mc_upgrade.sh
@@ -1,4 +1,4 @@
-SQLITE=sqlite3mc-2.3.3-sqlite-3.53.0-amalgamation
+SQLITE=sqlite3mc-2.3.5-sqlite-3.53.2-amalgamation
 curl -L https://github.com/utelle/SQLite3MultipleCiphers/releases/latest/download/$SQLITE.zip > $SQLITE.zip
 unzip -p "$SQLITE.zip" "sqlite3mc_amalgamation.c" > "sqlite3mc/sqlite3mc_amalgamation.c"
 unzip -p "$SQLITE.zip" "sqlite3mc_amalgamation.h" > "sqlite3mc/sqlite3mc_amalgamation.h"
